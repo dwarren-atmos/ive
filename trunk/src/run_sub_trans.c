7cb7bfbab1c15dd227b71013b34f82409e3c84cf
warren <warren@4d969c67-8336-0410-9cd7-94955585d1e5>
2007-09-17 21:27:56 +0000

    include string.h and malloc.h declare run_sub_

--- trunk/src/run_sub_trans.c
+++ trunk/src/run_sub_trans.c
@@ -1,10 +1,16 @@
+#include <string.h>
 #ifdef MEMDBG
 #include <mnemosyne.h>
+#else
+#include <malloc.h>
 #endif
 #include <stdio.h>
 #include <ive_macros.h>
 static int not_window=0; /*flag passed to driver; means do update windows*/
 static int zero=0, one=1;
+
+extern void make_help_widget_(),run_sub_();
+
 int
 run_sub_trans_(iflag)
 
@@ -13,7 +19,6 @@ run_sub_trans_(iflag)
 {
   char  buf[256], **argv, **get_args();
   int argc;
-  
   argv = get_args(&argc);
   if (argc != 2) {
     sprintf(buf, "Command %s requires 1 value", argv[0]);
