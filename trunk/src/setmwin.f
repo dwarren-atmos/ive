716b8927650f115f2f752ae733b0556014f3dfd9
reinecke <reinecke@4d969c67-8336-0410-9cd7-94955585d1e5>
2007-09-19 18:12:32 +0000

    map_window=lon1,lon2,lat1,lat2 works now.

--- trunk/src/setmwin.f
+++ trunk/src/setmwin.f
@@ -17,6 +17,7 @@ c-----------------------------------------------------------------------
 c
       include 'incommands.icl'
       include 'message.icl'
+      include 'units.icl'
       include 'supma.icl'
       include 'ive_widgets.icl'
 c
@@ -26,16 +27,24 @@ c
 c
 c     Local variable declarations.
 c
-      integer       i, numval
+      real :: mapwin_tmp(4),u1,u2,v1,v2
+      integer       i, ii, numval
+      character(len=80) :: wincmd
+      logical mapflg
 c
 c     External function declarations.
 c
       integer nblank, strbeg, strend
 c
-c
-c
-c
       error = .false.
+
+      call getlvar('mapflg', mapflg, error)
+      if(.not.mapflg.or.error) then
+        error=.true.
+        write (message,130) trim(comand(1))
+        call write_message
+        goto 90
+      end if
 c
 c     Get the first value for the current command.
 c
@@ -43,85 +52,47 @@ c
       do 5 i = fstval, 5
          if ( nblank(comand(i)) .ne. 0 )  numval = numval + 1
  5    continue
+
       if(numval .lt. 4 ) then
-         error = 1
-         write (message,*)
-     &        'map_window requires 8 arguments (u1 v1 u2 v2)'
+         error = .true.
+         write (message,'(A,1x,A)')
+     &       'map_window requires 4 arguments',
+     &       '(map_window=lon_ll,lon_ur,lat_ll,lat_ur)'
          call write_message
-         return
-      endif
-      
-      
-c
-c
-c     Make sure a value is given.
-c
-      call cvatof ( comand(fstval)(strbeg(comand(fstval)):
-     &     strend(comand(fstval))),
-     &     mapwin_u1, error) 
-      if ( error ) then
-         write (message, 110) comand(fstval)(strbeg(comand(fstval)):
-     &        strend(comand(fstval)))
-     &        ,comand(1)(strbeg(comand(1)):strend(comand(1)))
-         call write_message
-         mapwin_u1=0.0
-         mapwin_u2=0.0
-         mapwin_v1=0.0
-         mapwin_v2=0.0
-         return
+         goto 90
       endif
 
-      call cvatof ( comand(fstval+1)(strbeg(comand(fstval+1)):
-     &     strend(comand(fstval+1))),
-     &     mapwin_v1, error) 
-      if ( error ) then
-         write (message, 110) comand(fstval+1)(strbeg(comand(fstval+1)):
-     &        strend(comand(fstval+1)))
-     &        ,comand(1)(strbeg(comand(1)):strend(comand(1)))
-         mapwin_u1=0.0
-         mapwin_u2=0.0
-         mapwin_v1=0.0
-         mapwin_v2=0.0
-         call write_message
-         return
-      endif
-      
-      call cvatof ( comand(fstval+2)(strbeg(comand(fstval+2)):
-     &     strend(comand(fstval+2))),
-     &     mapwin_u2, error) 
-      if ( error ) then
-         write (message, 110) comand(fstval+2)(strbeg(comand(fstval+2)):
-     &        strend(comand(fstval+2)))
-     &        ,comand(1)(strbeg(comand(1)):strend(comand(1)))
-         mapwin_u1=0.0
-         mapwin_u2=0.0
-         mapwin_v1=0.0
-         mapwin_v2=0.0
-         call write_message
-         return
-      endif
-      
-      call cvatof ( comand(fstval+3)(strbeg(comand(fstval+3)):
-     &     strend(comand(fstval+3))),
-     &     mapwin_v2, error) 
-      if ( error ) then
-         write (message, 110) comand(fstval+3)(strbeg(comand(fstval+3)):
-     &        strend(comand(fstval+3)))
-     &        ,comand(1)(strbeg(comand(1)):strend(comand(1)))
-         mapwin_u1=0.0
-         mapwin_u2=0.0
-         mapwin_v1=0.0
-         mapwin_v2=0.0
-         call write_message
-         return
-      endif
-      
-c
-c     Format statements.
-c
+      ! Make sure a values is given
+      do i=1,4 
+        ii=fstval+i-1
+        call cvatof(comand(ii)(strbeg(comand(ii)):strend(comand(ii))+1),
+     &                         mapwin_tmp(i),error)
+        if(error) then
+          write (message, 110) trim(comand(ii)), trim(comand(1))
+          call write_message
+          goto 90
+        end if
+      end do
+
+      mapwin_u1=mapwin_tmp(1) ; mapwin_u2=mapwin_tmp(2) 
+      mapwin_v1=mapwin_tmp(3) ; mapwin_v2=mapwin_tmp(4) 
+      call lonlat_2_phys_trans(u1, v1, mapwin_u1, mapwin_v1, 1)
+      call lonlat_2_phys_trans(u2, v2, mapwin_u2, mapwin_v2, 1)
+
+      call scale(u1, 1, domain_slope(1), domain_intercept(1), 0.0)
+      call scale(u2, 1, domain_slope(1), domain_intercept(1), 0.0)
+      call scale(v1, 1, domain_slope(2), domain_intercept(2), 0.0)
+      call scale(v2, 1, domain_slope(2), domain_intercept(2), 0.0)
+      write(wincmd,120) u1,u2,v1,v2
+      call driver(wincmd,1)
+
+ 90   mapwin_u1=0.0 ; mapwin_u2=0.0 ; mapwin_v1=0.0 ; mapwin_v2=0.0
+
+
  100  format (1x,'Command ',a,' requires a value.')
  110  format (1x,a,' is an incorrect value for command ',a,'.')
-c
-c
+ 120  format ('window=',3(F14.4,','),F14.4)
+ 130  format ('You must be on a map to use ',A)
+
       return
       end
