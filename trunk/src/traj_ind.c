711f09b7567a2f1e838ab7e731ccbc39206089bd
warren <warren@4d969c67-8336-0410-9cd7-94955585d1e5>
2008-08-29 21:47:17 +0000

    Clean up to make like traj

--- trunk/src/traj_ind.c
+++ trunk/src/traj_ind.c
@@ -4,29 +4,40 @@ extern int checkoutbounds();
 extern void getrarr_(),make_help_widget_(),huen_(),getivar_();
 static float wmin[4], wmax[4], dmin[4], dmax[4];
 static int xyaxis[2];
-
-int traj_ind_(u,v,w, x, y, z, t, unx,uny,unz,unt,
+#define MAXPTS 500
+int traj_ind_(u,v,w, x, y, z, t, bfield, unx,uny,unz,unt,
 	      vnx,vny,vnz,vnt,wnx,wny,wnz,wnt)
      int  *unx,*uny,*unz,*unt, *vnx,*vny,*vnz,*vnt,*wnx,*wny,*wnz,*wnt;
-     float *u, *v, *w, **x, **y, **z, **t;
+     float *u, *v, *w, **x, **y, **z, **t, **bfield;
 {
   extern struct {float start[4];}traj_start_;
   extern struct {float ustag[4],  vstag[4], wstag[4];}traj_stag_;
   extern struct {char att_var_traj[80], traj_component[3][80], 
 		   traj_units[3][80];}traj_pointers_;
-  int i, length, num_pts, dims;
+  int i, length, num_pts, dims,grounded, bx, by, bz, bt, bg, error;
   float phys[4], *tmp_cord, *flip, delt, *x_cord, *y_cord, *z_cord, *t_cord;
+  float *bgptr;
+  char traj_background[80];
   
+
   dims=4;
   (void)getrarr_("plwmin",wmin,&dims,&i,6,4);
   (void)getrarr_("plwmax",wmax,&dims,&i,6,4);
   (void)getrarr_("phmin",dmin,&dims,&i,6,4);
   (void)getrarr_("phmax",dmax,&dims,&i,6,4);
 
-  (void)getivar_("xyaxis",&xyaxis[0],&i,5);
-  (void)getivar_("xyaxis",&xyaxis[1],&i,5);
+  (void)getivar_("xaxis",&xyaxis[0],&i,5);
+  (void)getivar_("yaxis",&xyaxis[1],&i,5);
   xyaxis[0]=xyaxis[0]-1; xyaxis[1]=xyaxis[1]-1;
 
+  bg=0;
+  (void)getavar_("traj_background_field",traj_background,&i,21,80);
+  if(traj_background[0] != '\0' && traj_background[0] != ' '&&
+     strncasecmp(traj_background,"depth",5)){
+    (void)get_pointer_(traj_background, &bgptr, &bx,&by,&bz,&bt,
+                       (int)strlen(traj_background));
+    bg=1;
+  }
   /* start with 2*time steps points*/
   
   length = 2*sizeof(float)*(*unt);
@@ -67,9 +78,12 @@ int traj_ind_(u,v,w, x, y, z, t, unx,uny,unz,unt,
   
   /*Backwards integration*/
   delt = -100;
-  while(!checkoutbounds(phys, dmin, dmax)){
+  grounded = 0;
+  /* arbitrary choice of 500 max points to avoid infinite loop*/
+  while(!checkoutbounds(phys, dmin, dmax) && !grounded && num_pts < MAXPTS){
     (void)huen_(phys, u, v, w, &dims, unx, uny, unz, unt,
-		vnx, wny, vnz, vnt, wnx, wny, wnz, wnt, &delt);
+		vnx, wny, vnz, vnt, wnx, wny, wnz, wnt, &grounded,
+		&delt, &error);
     if(num_pts == length/sizeof(float)){
       length = length +  2*sizeof(float)*(*unt);
       if((tmp_cord=(float *)realloc(x_cord,length)) == NULL){
@@ -155,13 +169,15 @@ int traj_ind_(u,v,w, x, y, z, t, unx,uny,unz,unt,
   
   /*Forwards integration*/
   delt = 100;
+  grounded = 0;
   phys[0]=traj_start_.start[0];
   phys[1]=traj_start_.start[1];
   phys[2]=traj_start_.start[2];
   phys[3]=traj_start_.start[3];
-  while(!checkoutbounds(phys, dmin, dmax)){
+  while(!checkoutbounds(phys, dmin, dmax) && !grounded && num_pts < MAXPTS){
     (void)huen_(phys, u, v, w, &dims, unx, uny, unz, unt,
-		vnx, wny, vnz, vnt, wnx, wny, wnz, wnt, &delt);
+		vnx, wny, vnz, vnt, wnx, wny, wnz, wnt, &grounded,
+		&delt, &error);
     if(num_pts == length/sizeof(float)){
       length = length +  2*sizeof(float)*(*unt);
       if((tmp_cord=(float *)realloc(x_cord,length)) == NULL){
@@ -210,6 +226,37 @@ int traj_ind_(u,v,w, x, y, z, t, unx,uny,unz,unt,
   *y = y_cord;
   *z = z_cord;
   *t = t_cord;
+  if(bg){
+    float special, *bf;
+    bf = (float *)malloc(num_pts * sizeof(float));
+    if(bf==(float *)NULL)
+      make_help_widget_("couldn't get background color points");
+    else{
+      float pval[4],cval[4];
+      int need[4],one=1, four=4;
+      i=16;
+      (void)getrvar_("spval",&special,&error,5);
+      need[0]=need[1]=need[2]=need[3]=1;
+      for (i=0; i<num_pts; i++){
+        pval[0]=x_cord[i];
+        pval[1]=y_cord[i];
+        pval[2]=z_cord[i];
+        pval[3]=t_cord[i];
+        cval[0]=cval[1]=cval[2]=cval[3]=special;
+        phys_2_index_trans_(pval, cval, need, &four,  &one);
+        bf[i] = (float)interp_(bgptr,&bx,&by,&bz,&bt,
+			       &cval[0],&cval[1],
+			       &cval[2],&cval[3],
+			       &special);
+      }
+    }
+    *bfield = bf;
+  }
+  else
+    *bfield = (float *)NULL;
+  if(num_pts == MAXPTS-1)
+    (void)make_help_widget_
+      ("Exceded MAXPTS in tradjectory - probably in loop");
   return(num_pts);
 }
 
